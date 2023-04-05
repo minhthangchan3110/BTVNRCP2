35f4b8e05d30db0d50d64e458df891b4c453beff
minhthangchan3110 <minhthang3916@gmail.com>
2023-04-05 21:08:59 +0700

    first commit

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1>My Website Header</h1>
+        <nav>
+        <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+    </ul>
+    </nav>
+    </header>
+  )
+}
